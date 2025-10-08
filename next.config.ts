d3fb47f27083f6a434432e37f53ee3659c539e37
Manu <mandrescabral@gmail.com>
2025-10-08 11:34:13 -0300

    refactor: simplify image remotePatterns configuration

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "imgar.zonapropcdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "imgar.argenprop.com",
-      },
-      {
-        protocol: "https",
-        hostname: "argenprop.com",
-      },
+      { protocol: "https", hostname: "imgar.zonapropcdn.com" },
+      { protocol: "https", hostname: "imgar.argenprop.com" },
+      { protocol: "https", hostname: "www.argenprop.com" },
+      { protocol: "https", hostname: "argenprop.com" },
     ],
   },
 };
